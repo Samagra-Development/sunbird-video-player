faccf13cf969b054c7cb4993292e7ed3c1350b3a
Bharat Kashyap <bharatkashyap@outlook.com>
2021-07-29 10:43:03 +0530

    feat: get streamingUrl and artifactUrl from query params

--- projects/sunbird-video-player/src/lib/components/video-player/video-player.component.ts
+++ projects/sunbird-video-player/src/lib/components/video-player/video-player.component.ts
@@ -1,10 +1,20 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild, ViewEncapsulation, OnDestroy, EventEmitter, Output } from '@angular/core';
-import { ViewerService } from '../../services/viewer.service';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation,
+  OnDestroy,
+  EventEmitter,
+  Output,
+} from "@angular/core";
+import { ViewerService } from "../../services/viewer.service";
 @Component({
-  selector: 'video-player',
-  templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "video-player",
+  templateUrl: "./video-player.component.html",
+  styleUrls: ["./video-player.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
   @Output() questionSetData = new EventEmitter();
@@ -14,11 +24,11 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
   showPlayButton = true;
   showPauseButton = false;
   showControls = true;
-  currentPlayerState = 'none';
+  currentPlayerState = "none";
   private unlistenTargetMouseMove: () => void;
   private unlistenTargetTouchStart: () => void;
-  @ViewChild('target', { static: true }) target: ElementRef;
-  @ViewChild('controlDiv', { static: true }) controlDiv: ElementRef;
+  @ViewChild("target", { static: true }) target: ElementRef;
+  @ViewChild("controlDiv", { static: true }) controlDiv: ElementRef;
   player: any;
   totalSeekedLength = 0;
   previousTime = 0;
@@ -29,52 +39,64 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
   totalSpentTime = 0;
   isAutoplayPrevented = false;
 
-  constructor(public viewerService: ViewerService, private renderer2: Renderer2) { }
+  constructor(
+    public viewerService: ViewerService,
+    private renderer2: Renderer2
+  ) {}
 
   ngAfterViewInit() {
-    this.viewerService.getPlayerOptions().then(options => {
-      this.player = videojs(this.target.nativeElement, {
-        fluid: true,
-        sources: options,
-        autoplay: true,
-        playbackRates: [0.5, 1, 1.5, 2],
-        controlBar: {
-          children: ['playToggle', 'volumePanel', 'durationDisplay',
-            'progressControl', 'remainingTimeDisplay',
-            'playbackRateMenuButton', 'fullscreenToggle']
-        }
-      }, function onLoad() {
-
-      });
-      const markers = this.viewerService.getMarkers()
+    this.viewerService.getPlayerOptions().then((options) => {
+      this.player = videojs(
+        this.target.nativeElement,
+        {
+          fluid: true,
+          sources: options,
+          autoplay: true,
+          playbackRates: [0.5, 1, 1.5],
+          controlBar: {
+            children: [
+              "playToggle",
+              "volumePanel",
+              "durationDisplay",
+              "progressControl",
+              "remainingTimeDisplay",
+              "playbackRateMenuButton",
+              "fullscreenToggle",
+            ],
+          },
+        },
+        function onLoad() {}
+      );
+      const markers = this.viewerService.getMarkers();
       if (markers) {
         this.player.markers({
           markers,
           markerStyle: {
-            'height': '7px',
-            'bottom': '39%',
-            'background-color': 'orange'
+            height: "7px",
+            bottom: "39%",
+            "background-color": "orange",
           },
           onMarkerReached: (marker) => {
-            if(marker){
+            if (marker) {
               const { time, text, identifier, duration } = marker;
-              if (!(this.player.currentTime() > (time + duration))) {
+              if (!(this.player.currentTime() > time + duration)) {
                 setTimeout(() => {
-                    this.pause()
-                    this.player.controls(false);  
+                  this.pause();
+                  this.player.controls(false);
                 }, 1000);
                 this.viewerService.getQuestionSet(identifier).subscribe(
                   (response) => {
-                    this.questionSetData.emit({response, time, identifier});
-                  }, (error) => {
-                    this.play()
+                    this.questionSetData.emit({ response, time, identifier });
+                  },
+                  (error) => {
+                    this.play();
                     this.player.controls(true);
-                    console.log(error); 
+                    console.log(error);
                   }
                 );
               }
             }
-          }
+          },
         });
         this.playerInstance.emit(this.player);
         this.viewerService.playerInstance = this.player;
@@ -84,87 +106,110 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
     });
 
     setInterval(() => {
-      if (!this.isAutoplayPrevented && this.currentPlayerState !== 'pause') {
+      if (!this.isAutoplayPrevented && this.currentPlayerState !== "pause") {
         this.showControls = false;
       }
     }, 5000);
 
-    this.unlistenTargetMouseMove = this.renderer2.listen(this.target.nativeElement, 'mousemove', () => {
-      this.showControls = true;
-    });
-    this.unlistenTargetTouchStart = this.renderer2.listen(this.target.nativeElement, 'touchstart', () => {
-      this.showControls = true;
-    });
+    this.unlistenTargetMouseMove = this.renderer2.listen(
+      this.target.nativeElement,
+      "mousemove",
+      () => {
+        this.showControls = true;
+      }
+    );
+    this.unlistenTargetTouchStart = this.renderer2.listen(
+      this.target.nativeElement,
+      "touchstart",
+      () => {
+        this.showControls = true;
+      }
+    );
 
-    this.viewerService.sidebarMenuEvent.subscribe(event => {
-      if (event === 'OPEN_MENU') { this.pause(); }
-      if (event === 'CLOSE_MENU') { this.play(); }
+    this.viewerService.sidebarMenuEvent.subscribe((event) => {
+      if (event === "OPEN_MENU") {
+        this.pause();
+      }
+      if (event === "CLOSE_MENU") {
+        this.play();
+      }
     });
   }
 
   registerEvents() {
     const promise = this.player.play();
     if (promise !== undefined) {
-      promise.catch(error => {
+      promise.catch((error) => {
         this.isAutoplayPrevented = true;
       });
     }
 
-    const events = ['loadstart', 'play', 'pause', 'durationchange',
-      'error', 'playing', 'progress', 'seeked', 'seeking', 'volumechange',
-      'ratechange'];
+    const events = [
+      "loadstart",
+      "play",
+      "pause",
+      "durationchange",
+      "error",
+      "playing",
+      "progress",
+      "seeked",
+      "seeking",
+      "volumechange",
+      "ratechange",
+    ];
 
-    this.player.on('fullscreenchange', (data) => {
+    this.player.on("fullscreenchange", (data) => {
       // This code is to show the controldiv in fullscreen mode
       if (this.player.isFullscreen()) {
-        this.target.nativeElement.parentNode.appendChild(this.controlDiv.nativeElement);
+        this.target.nativeElement.parentNode.appendChild(
+          this.controlDiv.nativeElement
+        );
       }
-      this.viewerService.raiseHeartBeatEvent('FULLSCREEN');
-    })
+      this.viewerService.raiseHeartBeatEvent("FULLSCREEN");
+    });
 
-    this.player.on('pause', (data) => {
+    this.player.on("pause", (data) => {
       this.pause();
     });
 
-    this.player.on('play', (data) => {
-      this.currentPlayerState = 'play';
+    this.player.on("play", (data) => {
+      this.currentPlayerState = "play";
       this.showPauseButton = true;
       this.showPlayButton = false;
-      this.viewerService.raiseHeartBeatEvent('PLAY');
+      this.viewerService.raiseHeartBeatEvent("PLAY");
       this.isAutoplayPrevented = false;
     });
 
-    this.player.on('timeupdate', (data) => {
+    this.player.on("timeupdate", (data) => {
       this.handleVideoControls(data);
       this.viewerService.playerEvent.emit(data);
       if (this.player.currentTime() === this.player.duration()) {
-        this.handleVideoControls({ type: 'ended' });
-        this.viewerService.playerEvent.emit({ type: 'ended' });
+        this.handleVideoControls({ type: "ended" });
+        this.viewerService.playerEvent.emit({ type: "ended" });
       }
     });
-    events.forEach(event => {
+    events.forEach((event) => {
       this.player.on(event, (data) => {
         this.handleVideoControls(data);
         this.viewerService.playerEvent.emit(data);
       });
     });
-
   }
 
   toggleForwardRewindButton() {
     this.showForwardButton = true;
     this.showBackwardButton = true;
-    if ((this.player.currentTime() + this.time) > this.player.duration()) {
+    if (this.player.currentTime() + this.time > this.player.duration()) {
       this.showForwardButton = false;
     }
-    if ((this.player.currentTime() - this.time) < 0) {
+    if (this.player.currentTime() - this.time < 0) {
       this.showBackwardButton = false;
     }
   }
 
   play() {
     this.player.play();
-    this.currentPlayerState = 'play';
+    this.currentPlayerState = "play";
     this.showPauseButton = true;
     this.showPlayButton = false;
     this.toggleForwardRewindButton();
@@ -172,77 +217,86 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
 
   pause() {
     this.player.pause();
-    this.currentPlayerState = 'pause';
+    this.currentPlayerState = "pause";
     this.showPauseButton = false;
     this.showPlayButton = true;
     this.toggleForwardRewindButton();
-    this.viewerService.raiseHeartBeatEvent('PAUSE');
+    this.viewerService.raiseHeartBeatEvent("PAUSE");
   }
 
   backward() {
     this.player.currentTime(this.player.currentTime() - this.time);
     this.toggleForwardRewindButton();
-    this.viewerService.raiseHeartBeatEvent('BACKWARD');
+    this.viewerService.raiseHeartBeatEvent("BACKWARD");
   }
 
   forward() {
     this.player.currentTime(this.player.currentTime() + this.time);
     this.toggleForwardRewindButton();
-    this.viewerService.raiseHeartBeatEvent('FORWARD');
+    this.viewerService.raiseHeartBeatEvent("FORWARD");
   }
 
   handleVideoControls({ type }) {
-    if (type === 'playing') {
+    if (type === "playing") {
       this.showPlayButton = false;
       this.showPauseButton = true;
     }
-    if (type === 'ended') {
+    if (type === "ended") {
       this.totalSpentTime += new Date().getTime() - this.startTime;
       this.viewerService.visitedLength = this.totalSpentTime;
       this.viewerService.currentlength = this.player.currentTime();
       this.viewerService.totalLength = this.player.duration();
       this.updatePlayerEventsMetadata({ type });
     }
-    if (type === 'pause') {
+    if (type === "pause") {
       this.totalSpentTime += new Date().getTime() - this.startTime;
       this.updatePlayerEventsMetadata({ type });
     }
-    if (type === 'play') {
+    if (type === "play") {
       this.startTime = new Date().getTime();
       this.updatePlayerEventsMetadata({ type });
     }
 
-    if (type === 'loadstart') {
+    if (type === "loadstart") {
       this.startTime = new Date().getTime();
     }
 
     // Calulating total seeked length
-    if (type === 'timeupdate') {
+    if (type === "timeupdate") {
       this.previousTime = this.currentTime;
       this.currentTime = this.player.currentTime();
       this.toggleForwardRewindButton();
     }
-    if (type === 'seeking') {
-      if (this.seekStart === null) { this.seekStart = this.previousTime; }
+    if (type === "seeking") {
+      if (this.seekStart === null) {
+        this.seekStart = this.previousTime;
+      }
     }
-    if (type === 'seeked') {
+    if (type === "seeked") {
       this.updatePlayerEventsMetadata({ type });
       if (this.currentTime > this.seekStart) {
-        this.totalSeekedLength = this.totalSeekedLength + (this.currentTime - this.seekStart);
+        this.totalSeekedLength =
+          this.totalSeekedLength + (this.currentTime - this.seekStart);
       } else if (this.seekStart > this.currentTime) {
-        this.totalSeekedLength = this.totalSeekedLength + (this.seekStart - this.currentTime);
+        this.totalSeekedLength =
+          this.totalSeekedLength + (this.seekStart - this.currentTime);
       }
       this.viewerService.totalSeekedLength = this.totalSeekedLength;
       this.seekStart = null;
-      if(this.player.markers && this.player.markers.getMarkers) {
-        const markers = this.player.markers.getMarkers()
-        markers.forEach(marker => {
-          if(!this.viewerService.interceptionResponses[marker.time] && marker.time < this.currentTime) {
+      if (this.player.markers && this.player.markers.getMarkers) {
+        const markers = this.player.markers.getMarkers();
+        markers.forEach((marker) => {
+          if (
+            !this.viewerService.interceptionResponses[marker.time] &&
+            marker.time < this.currentTime
+          ) {
             this.viewerService.interceptionResponses[marker.time] = {
               score: 0,
-              isSkipped: false
-            }
-            document.querySelector(`[data-marker-time="${marker.time}"]`)['style'].backgroundColor = "red";
+              isSkipped: false,
+            };
+            document.querySelector(`[data-marker-time="${marker.time}"]`)[
+              "style"
+            ].backgroundColor = "red";
           }
         });
       }
@@ -254,7 +308,7 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
     this.viewerService.metaData.playBackSpeeds.push(this.player.playbackRate());
     this.viewerService.metaData.volume.push(this.player.volume());
     const action = {};
-    action[type + ''] = this.player.currentTime();
+    action[type + ""] = this.player.currentTime();
     this.viewerService.metaData.actions.push(action);
   }
   ngOnDestroy() {
--- projects/sunbird-video-player/src/lib/sunbird-video-player.component.ts
+++ projects/sunbird-video-player/src/lib/sunbird-video-player.component.ts
@@ -1,25 +1,41 @@
 import {
-  ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output,
-  HostListener, ElementRef, ViewChild, AfterViewInit, Renderer2, OnDestroy
-} from '@angular/core';
-import { ErrorService , errorCode , errorMessage } from '@project-sunbird/sunbird-player-sdk-v9';
-import { identity } from 'rxjs';
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  HostListener,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  Renderer2,
+  OnDestroy,
+} from "@angular/core";
+import {
+  ErrorService,
+  errorCode,
+  errorMessage,
+} from "@project-sunbird/sunbird-player-sdk-v9";
+import { identity } from "rxjs";
 
-import { PlayerConfig } from './playerInterfaces';
-import { ViewerService } from './services/viewer.service';
-import { SunbirdVideoPlayerService } from './sunbird-video-player.service';
+import { PlayerConfig } from "./playerInterfaces";
+import { ViewerService } from "./services/viewer.service";
+import { SunbirdVideoPlayerService } from "./sunbird-video-player.service";
 @Component({
-  selector: 'sunbird-video-player',
-  templateUrl: './sunbird-video-player.component.html',
-  styleUrls: ['./sunbird-video-player.component.scss']
+  selector: "sunbird-video-player",
+  templateUrl: "./sunbird-video-player.component.html",
+  styleUrls: ["./sunbird-video-player.component.scss"],
 })
-export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
-
+export class SunbirdVideoPlayerComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   @Input() playerConfig: PlayerConfig;
   @Output() playerEvent: EventEmitter<object>;
+  @Input() streamingUrl: string;
   @Output() telemetryEvent: EventEmitter<any> = new EventEmitter<any>();
-  @ViewChild('videoPlayer', { static: true }) videoPlayerRef: ElementRef;
-  viewState = 'player';
+  @ViewChild("videoPlayer", { static: true }) videoPlayerRef: ElementRef;
+  viewState = "player";
   public traceId: string;
   public nextContent: any;
   showContentError: boolean;
@@ -28,13 +44,13 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
     showShare: true,
     showDownload: true,
     showReplay: true,
-    showExit: true
+    showExit: true,
   };
   private unlistenTouchStart: () => void;
   private unlistenMouseMove: () => void;
   isPaused = false;
   showQumlPlayer = false;
-  QumlPlayerConfig :any = {};
+  QumlPlayerConfig: any = {};
   videoInstance: any;
   currentInterceptionTime;
   currentInterceptionUIId;
@@ -47,38 +63,46 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
     public errorService: ErrorService
   ) {
     this.playerEvent = this.viewerService.playerEvent;
-    this.viewerService.playerEvent.subscribe(event => {
-      if(event.type === 'pause') {
+    this.viewerService.playerEvent.subscribe((event) => {
+      if (event.type === "pause") {
         this.isPaused = true;
         this.showControls = true;
       }
-      if(event.type === 'play') {
+      if (event.type === "play") {
         this.isPaused = false;
       }
-      if (event.type === 'loadstart') {
+      if (event.type === "loadstart") {
         this.viewerService.raiseStartEvent(event);
       }
-      if (event.type === 'ended') {
+      if (event.type === "ended") {
         this.viewerService.endPageSeen = true;
         this.viewerService.raiseEndEvent();
-        this.viewState = 'end';
+        this.viewState = "end";
       }
-      if (event.type === 'error') {
+      if (event.type === "error") {
         let code = errorCode.contentLoadFails,
-          message = errorMessage.contentLoadFails
+          message = errorMessage.contentLoadFails;
         if (this.viewerService.isAvailableLocally) {
-            code = errorCode.contentLoadFails,
-            message = errorMessage.contentLoadFails
+          (code = errorCode.contentLoadFails),
+            (message = errorMessage.contentLoadFails);
         }
         if (code === errorCode.contentLoadFails) {
           this.showContentError = true;
         }
-        this.viewerService.raiseExceptionLog(code, message, event, this.traceId);
-
+        this.viewerService.raiseExceptionLog(
+          code,
+          message,
+          event,
+          this.traceId
+        );
       }
-      const events = [{ type: 'volumechange', telemetryEvent: 'VOLUME_CHANGE' }, { type: 'seeking', telemetryEvent: 'DRAG' }, { type: 'fullscreen', telemetryEvent: 'FULLSCREEN' },
-      { type: 'ratechange', telemetryEvent: 'RATE_CHANGE' }];
-      events.forEach(data => {
+      const events = [
+        { type: "volumechange", telemetryEvent: "VOLUME_CHANGE" },
+        { type: "seeking", telemetryEvent: "DRAG" },
+        { type: "fullscreen", telemetryEvent: "FULLSCREEN" },
+        { type: "ratechange", telemetryEvent: "RATE_CHANGE" },
+      ];
+      events.forEach((data) => {
         if (event.type === data.type) {
           this.viewerService.raiseHeartBeatEvent(data.telemetryEvent);
         }
@@ -86,7 +110,7 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
     });
   }
 
-  @HostListener('document:TelemetryEvent', ['$event'])
+  @HostListener("document:TelemetryEvent", ["$event"])
   onTelemetryEvent(event) {
     this.telemetryEvent.emit(event.detail);
   }
@@ -100,23 +124,36 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
 
     /* tslint:disable:no-string-literal */
     this.nextContent = this.playerConfig.config.nextContent;
-    this.traceId = this.playerConfig.config['traceId'];
-    this.sideMenuConfig = { ...this.sideMenuConfig, ...this.playerConfig.config.sideMenu };
+    this.traceId = this.playerConfig.config["traceId"];
+    this.sideMenuConfig = {
+      ...this.sideMenuConfig,
+      ...this.playerConfig.config.sideMenu,
+    };
+    console.log("inside video player component", this.playerConfig);
     this.viewerService.initialize(this.playerConfig);
     this.videoPlayerService.initialize(this.playerConfig);
-    window.addEventListener('offline', this.raiseInternetDisconnectionError , true);
+    window.addEventListener(
+      "offline",
+      this.raiseInternetDisconnectionError,
+      true
+    );
     this.QumlPlayerConfig.config = this.playerConfig.config;
     this.QumlPlayerConfig.config.sideMenu.enable = false;
     this.QumlPlayerConfig.context = this.playerConfig.context;
-    this.setTelemetryObjectRollup(this.playerConfig.metadata.identifier)
+    this.setTelemetryObjectRollup(this.playerConfig.metadata.identifier);
   }
 
   raiseInternetDisconnectionError = () => {
     let code = errorCode.internetConnectivity;
     let message = errorMessage.internetConnectivity;
     let stacktrace = `${code}: ${message}`;
-    this.viewerService.raiseExceptionLog(code, message, stacktrace, this.traceId);
-  }
+    this.viewerService.raiseExceptionLog(
+      code,
+      message,
+      stacktrace,
+      this.traceId
+    );
+  };
 
   sidebarMenuEvent(event) {
     this.viewerService.sidebarMenuEvent.emit(event);
@@ -124,99 +161,118 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
 
   ngAfterViewInit() {
     const videoPlayerElement = this.videoPlayerRef.nativeElement;
-    this.unlistenMouseMove = this.renderer2.listen(videoPlayerElement, 'mousemove', () => {
-      this.showControls = true;
-    });
+    this.unlistenMouseMove = this.renderer2.listen(
+      videoPlayerElement,
+      "mousemove",
+      () => {
+        this.showControls = true;
+      }
+    );
 
-    this.unlistenTouchStart = this.renderer2.listen(videoPlayerElement, 'touchstart', () => {
-      this.showControls = true;
-    });
+    this.unlistenTouchStart = this.renderer2.listen(
+      videoPlayerElement,
+      "touchstart",
+      () => {
+        this.showControls = true;
+      }
+    );
 
-    const contentCompabilityLevel = this.playerConfig.metadata['compatibilityLevel'];
+    const contentCompabilityLevel =
+      this.playerConfig.metadata["compatibilityLevel"];
     if (contentCompabilityLevel) {
-      const checkContentCompatible = this.errorService.checkContentCompatibility(contentCompabilityLevel);
-      if (!checkContentCompatible['isCompitable']) {
-        this.viewerService.raiseExceptionLog(errorCode.contentCompatibility, errorMessage.contentCompatibility, checkContentCompatible['error']['message'], this.traceId);
+      const checkContentCompatible =
+        this.errorService.checkContentCompatibility(contentCompabilityLevel);
+      if (!checkContentCompatible["isCompitable"]) {
+        this.viewerService.raiseExceptionLog(
+          errorCode.contentCompatibility,
+          errorMessage.contentCompatibility,
+          checkContentCompatible["error"]["message"],
+          this.traceId
+        );
       }
     }
   }
 
   sideBarEvents(event) {
     this.playerEvent.emit(event);
-    if (event === 'DOWNLOAD') {
+    if (event === "DOWNLOAD") {
       this.downloadVideo();
     }
-    const events = ['SHARE', 'DOWNLOAD_MENU', 'EXIT', 'CLOSE_MENU'];
-    events.forEach(data => {
+    const events = ["SHARE", "DOWNLOAD_MENU", "EXIT", "CLOSE_MENU"];
+    events.forEach((data) => {
       if (event === data) {
         this.viewerService.raiseHeartBeatEvent(data);
       }
-      if (event === 'EXIT') {
-        this.viewerService.sidebarMenuEvent.emit('CLOSE_MENU');
+      if (event === "EXIT") {
+        this.viewerService.sidebarMenuEvent.emit("CLOSE_MENU");
       }
     });
   }
 
   setTelemetryObjectRollup(id) {
-    if(this.QumlPlayerConfig.context) {
-      const hasObjectRollup = this.QumlPlayerConfig && this.QumlPlayerConfig.context && this.QumlPlayerConfig.context.objectRollup
-      if(!hasObjectRollup) {
-        this.QumlPlayerConfig.context.objectRollup = {}
+    if (this.QumlPlayerConfig.context) {
+      const hasObjectRollup =
+        this.QumlPlayerConfig &&
+        this.QumlPlayerConfig.context &&
+        this.QumlPlayerConfig.context.objectRollup;
+      if (!hasObjectRollup) {
+        this.QumlPlayerConfig.context.objectRollup = {};
       }
-      const levels = Object.keys(this.QumlPlayerConfig.context.objectRollup)
-      this.QumlPlayerConfig.context.objectRollup[`l${levels.length +  1}`] = id
+      const levels = Object.keys(this.QumlPlayerConfig.context.objectRollup);
+      this.QumlPlayerConfig.context.objectRollup[`l${levels.length + 1}`] = id;
     }
   }
 
-  playContent(event){
+  playContent(event) {
     this.viewerService.raiseHeartBeatEvent(event.type);
   }
 
   replayContent(event) {
     this.playerEvent.emit(event);
-    this.viewState = 'player';
-    this.viewerService.raiseHeartBeatEvent('REPLAY');
+    this.viewState = "player";
+    this.viewerService.raiseHeartBeatEvent("REPLAY");
   }
 
   exitContent(event) {
     this.playerEvent.emit(event);
-    this.viewerService.raiseHeartBeatEvent('EXIT');
+    this.viewerService.raiseHeartBeatEvent("EXIT");
   }
 
   downloadVideo() {
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = this.viewerService.artifactUrl;
     a.download = this.viewerService.contentName;
-    a.target = '_blank';
+    a.target = "_blank";
     document.body.appendChild(a);
     a.click();
     a.remove();
-    this.viewerService.raiseHeartBeatEvent('DOWNLOAD');
+    this.viewerService.raiseHeartBeatEvent("DOWNLOAD");
   }
 
-  
-
   qumlPlayerEvents(event) {
-    if(event.eid === "QUML_SUMMARY"){
-      const score = parseInt(event.edata.extra.find(p => p.id === "score")["value"],10)
+    if (event.eid === "QUML_SUMMARY") {
+      const score = parseInt(
+        event.edata.extra.find((p) => p.id === "score")["value"],
+        10
+      );
       this.viewerService.interceptionResponses[this.currentInterceptionTime] = {
         score,
-        isSkipped: false
-      }
-      document.querySelector(`[data-marker-time="${this.currentInterceptionTime}"]`)['style'].backgroundColor = "green";
+        isSkipped: false,
+      };
+      document.querySelector(
+        `[data-marker-time="${this.currentInterceptionTime}"]`
+      )["style"].backgroundColor = "green";
       this.showQumlPlayer = false;
       this.videoInstance.play();
       this.videoInstance.controls(true);
     }
   }
 
-
-  questionSetData({response, time,identifier}) {
-    
+  questionSetData({ response, time, identifier }) {
     this.QumlPlayerConfig.metadata = response;
-    this.QumlPlayerConfig.metadata['showStartPage'] = 'No';
-    this.QumlPlayerConfig.metadata['showEndPage'] = 'No';
-    this.currentInterceptionTime = time
+    this.QumlPlayerConfig.metadata["showStartPage"] = "No";
+    this.QumlPlayerConfig.metadata["showEndPage"] = "No";
+    this.currentInterceptionTime = time;
     this.currentInterceptionUIId = identifier;
     this.showQumlPlayer = true;
   }
@@ -225,11 +281,15 @@ export class SunbirdVideoPlayerComponent implements OnInit, AfterViewInit, OnDes
     this.videoInstance = event;
   }
 
-  @HostListener('window:beforeunload')
+  @HostListener("window:beforeunload")
   ngOnDestroy() {
     this.viewerService.raiseEndEvent();
     this.unlistenTouchStart();
     this.unlistenMouseMove();
-    window.removeEventListener('offline', this.raiseInternetDisconnectionError , true);
+    window.removeEventListener(
+      "offline",
+      this.raiseInternetDisconnectionError,
+      true
+    );
   }
 }
